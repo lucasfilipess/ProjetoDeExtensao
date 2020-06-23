80914f4c0dde658c389667550da4cb80e5ee9cd5
samaraduarte <51268179+samaraduarte@users.noreply.github.com>
2020-06-23 17:49:23 -0300

    Boxes Dashboard

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,13 +1,30 @@
 import React, { useEffect } from 'react';
-import {} from './styles.module.scss';
-
+import {Container, Boxes, Boxes2, Graphic, Calendar, Boxes3, Products, Orders} from './styles.module.scss';
+import calendar from '../../assets/images/calendar.png';
 function Dashboard({ setIsActive }) {
   useEffect(() => {
     setIsActive(1);
   }, [setIsActive]);
   return (
     <>
-      <h1>Dashboard</h1>
+    <div className={Container}>
+      <div className={Boxes}>
+        <div>Proxima Consulta</div>
+        <div>Consultas Agendadas</div>
+        <div>Consultas Passadas</div>
+        <div>Caixinha 4</div>
+      </div>
+      <div className={Boxes2}>
+        <div className={Graphic}></div>
+        <div className={Calendar}>
+          <img src={calendar} alt="calendar"/>
+        </div>
+      </div>
+      <div className={Boxes3}>
+        <div className={Products}></div>
+        <div className={Orders}></div>
+      </div>
+    </div> 
     </>
   );
 }
