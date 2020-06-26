af3940ced3f1d665e9410bf37b1fbdb108078f84
samaraduarte <51268179+samaraduarte@users.noreply.github.com>
2020-06-26 17:10:51 -0300

    Alteracoes e responsividade

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
-import {Container, Boxes, BlueBox, Boxes2, Warnings, Arrow, Calendar, Boxes3, SocialMedia, SocialMediaIcons, Graphic} from './styles.module.scss';
-import calendar from '../../assets/images/calendar.png';
-import calendarSvg from '../../assets/images/calendar.svg';
-import pills from '../../assets/images/kendal-L4iKccAChOc-unsplash.jpg';
-import next from '../../assets/images/next.svg';
-import next1 from '../../assets/images/next (1).svg';
-import calendarGoogle from '../../assets/images/googleAgenda.webp';
-import facebookIcon from '../../assets/images/facebook.svg';
-import gmailIcon from '../../assets/images/email.svg';
-import whatsappIcon from '../../assets/images/whatsapp.svg';
-import youtubeIcon from '../../assets/images/youtube.svg';
-import graphic from '../../assets/images/grafico.svg';
+import React, { useEffect } from "react";
+
+import {
+  Container,
+  Boxes,
+  BlueBox,
+  Boxes2,
+  Warnings,
+  Arrow,
+  Calendar,
+  Boxes3,
+  SocialMedia,
+  Graphic,
+} from "./styles.module.scss";
+import calendar from "../../assets/images/calendar.png";
+import calendarSvg from "../../assets/images/calendar.svg";
+import pills from "../../assets/images/kendal-L4iKccAChOc-unsplash.jpg";
+import calendarGoogle from "../../assets/images/googleAgenda.webp";
+import facebookIcon from "../../assets/images/facebook.svg";
+import gmailIcon from "../../assets/images/email.svg";
+import whatsappIcon from "../../assets/images/whatsapp.svg";
+import youtubeIcon from "../../assets/images/youtube.svg";
+import graphic from "../../assets/images/grafico.PNG";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 function Dashboard({ setIsActive }) {
   useEffect(() => {
@@ -18,63 +29,59 @@ function Dashboard({ setIsActive }) {
   }, [setIsActive]);
   return (
     <>
-    <div className={Container}>
-      <div className={Boxes}>
-        <div>
-          <h2>Proxima Consulta</h2>
-          <img src={calendarSvg} alt="calendar"/>
-          <p>8 de Julho (Quinta-feira)</p>
-          <p>Horario: 09:00</p>
-        </div>
-        <div>
-          <h2>Consultas Agendadas</h2>
-          <p>8 de Julho (Quinta-feira)</p>
-          <p>10 de Agosto (Segunda-feira)</p>
-          <p>17 de Julho (Segunda-feira)</p>
-        </div>
-        <div>
-          <h2>Consultas Passadas</h2>
-          <p>12 de Agosto (Terça-feira)</p>
-          <p>13 de Julho (Terça-feira)</p>
-        </div>
-        <div className={BlueBox}>
-          <h2>Especialidades</h2>
-          <p>Clinico Geral</p>
-          <p>Fisioterapeuta</p>
-          <p>Pesquisar</p>
-        </div>
-      </div>
-
-      <div className={Boxes2}>
-        <div className={Warnings}>
-          <img src={pills} alt="calendar"/>
-          <p>Saiba como jogar fora medicamentos vencidos</p>          
-          <div className={Arrow}>
-            <img src={next1} alt="arrow"/>
-            <img src={next} alt="arrow"/>
+      <div className={Container}>
+        <div className={Boxes}>
+          <div>
+            <h2>Proxima Consulta</h2>
+            <img src={calendarSvg} alt="calendar" />
+            <p>8 de Julho (Quinta-feira)</p>
+            <p>Horario: 09:00</p>
+          </div>
+          <div>
+            <h2>Consultas Agendadas</h2>
+            <p>8 de Julho (Quinta-feira)</p>
+            <p>17 de Julho (Segunda-feira)</p>
+          </div>
+          <div>
+            <h2>Consultas Passadas</h2>
+            <p>12 de Agosto (Terça-feira)</p>
+            <p>13 de Julho (Terça-feira)</p>
+          </div>
+          <div className={BlueBox}>
+            <h2>Especialidades</h2>
+            <p>Clinico Geral</p>
+            <p>Fisioterapeuta</p>
+            <p>Pesquisar</p>
           </div>
         </div>
-        <div className={Calendar}>
-          <img src={calendar} alt="calendar"/>
-        </div>
-      </div>
 
-      <div className={Boxes3}>
-        <div className={SocialMedia}>
-          <img src={calendarGoogle} alt="calendar"/>         
-        <button>Adicione suas consultas no Google Agenda</button>
-        <div className={SocialMediaIcons}>
-            <img src={facebookIcon} alt="social media"/>
-            <img src={gmailIcon} alt="social media"/>
-            <img src={whatsappIcon} alt="social media"/>
-            <img src={youtubeIcon} alt="social media"/>
+        <div className={Boxes2}>
+          <div className={Warnings} style={{ backgroundImage: `url(${pills})` }}>
+            <p>Saiba como jogar fora medicamentos vencidos</p>
+            <div className={Arrow}>
+              <AiOutlineArrowLeft />
+              <AiOutlineArrowRight />
+            </div>
+          </div>
+          <div className={Calendar}>
+            <img src={calendar} alt="calendar" />
+          </div>
         </div>
-        </div>
-        <div className={Graphic}>          
-          <img src={graphic} alt="grafico horario de pico"/> 
+
+        <div className={Boxes3}>
+          <div className={SocialMedia}>         
+              <img src={calendarGoogle} alt="calendar" />
+              <button>Adicione suas consultas no Google Agenda</button>          
+            <div>
+              <img src={facebookIcon} alt="social media" />
+              <img src={gmailIcon} alt="social media" />
+              <img src={whatsappIcon} alt="social media" />
+              <img src={youtubeIcon} alt="social media" />
+            </div>
+          </div>
+          <div className={Graphic}  style={{ backgroundImage: `url(${graphic})` }}></div>
         </div>
       </div>
-    </div> 
     </>
   );
 }
